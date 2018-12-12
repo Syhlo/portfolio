5f7fc13ebc65985db487ddfe0d82d287e19c8ac3
= <matt.0609@hotmail.com>
2018-12-12 06:31:57 -0500

    Starting filter logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,11 +12,12 @@ const control =
 
 controller.addEventListener('click', () => {
     sliderC.toggle('closed')
+    sliderC.toggle('opened')
     control.toggle('fa-chevron-down')
     control.toggle('fa-chevron-up')
 
     // Store locally
-    sliderOpen()
+    storeSliderState()
 })
 
 
@@ -26,6 +27,7 @@ controller.addEventListener('click', () => {
 // #-----------------------------#
 const anchors =
     document.querySelectorAll('.navbar-menu .navbar-end .navbar-item')
+
 anchors.forEach((anchor) =>
     anchor.addEventListener('click', activeNavLink))
 
@@ -40,25 +42,21 @@ function activeNavLink(e) {
 
 
 
-// #-----------------------------#
-// #  Filter select and storage  #
-// #-----------------------------#
-const filter =
+// #------------------#
+// #  Select filters  #
+// #------------------#
+const filters =
     document.querySelectorAll('.filter')
-const tags =
-    document.querySelector('.is-tags')
 let storeFilters =
     JSON.parse(localStorage.getItem('selected')) || []
-// Clear Filters button
-document.querySelector('.clear-filters').addEventListener('click', clearFilters)
 
 // Filter select logic
-function filterSelect() {
-    for (let i = 0; i < filter.length; i++) {
+function filter() {
+    for (let i = 0; i < filters.length; i++) {
         // Set selected on reload
-        setSelected(i, filter[i])
+        setSelected(i, filters[i])
 
-        filter[i].addEventListener('click', () => {
+        filters[i].addEventListener('click', () => {
             // Set selected
             const target =
                 event.currentTarget
@@ -66,11 +64,86 @@ function filterSelect() {
 
             // Store locally
             storeSelected(i, target)
+            _filter(i)
         })
     }
 }
 
-// Store locally
+// Initiate
+filter()
+
+
+
+// #-----------------------#
+// #  Filter the Projects  #
+// #-----------------------#
+const projects = {
+    portfolio: [0, 1, 2, 4],
+    reactApp: [5],
+    anotherApp: [5],
+    lastProject: []
+}
+
+// Filtered IDs
+let filtered = []
+
+// Index of projects on site
+let project = document.getElementsByName('project');
+
+function _filter(index) {
+    // Manage filtered results
+    if (index === filtered[index]) {
+        filtered[index] = NaN
+
+    } else {
+        filtered[index] = index
+        showProjects(index)
+        hideProjects(index)
+    }
+
+    console.log(filtered)
+
+
+}
+
+function hideProjects(index) {
+    // For each project in projects
+    Object.keys(projects).forEach((e, i) => {
+        // if the project's array does not contain every element from filtered array & filtered[index] is not NaN
+        if (!compareArrays(projects[e], filtered) && filtered[index] === index) {
+            // filter those items out
+            project[i].classList.toggle('filtered')
+        }
+    })
+}
+
+function showProjects(index) {
+    Object.keys(projects).forEach((e, i) => {
+        if (!filtered[index] === index && project[i].classList.contains('filtered')) {
+            project[i].classList.toggle('filtered')
+        }
+    })
+}
+
+function compareArrays(superset, subset) {
+    if (0 === subset.length) {
+        return false;
+    }
+    return subset.every(function (value) {
+        return (superset.indexOf(value) >= 0);
+    });
+}
+
+
+
+// #-----------------#
+// #  Local Storage  #
+// #-----------------#
+// Clear filters button
+document.querySelector('.clear-filters').addEventListener('click', clearFilters)
+
+
+// Selected Filters
 function storeSelected(index, target) {
     const item =
         JSON.stringify({
@@ -81,8 +154,9 @@ function storeSelected(index, target) {
     localStorage.setItem('selected', JSON.stringify(storeFilters))
 }
 
-// Save whether slider is open
-function sliderOpen() {
+
+// Slider State
+function storeSliderState() {
     const item = JSON.stringify({
         sliderOpen:
             !sliderC.contains('closed')
@@ -91,27 +165,28 @@ function sliderOpen() {
     localStorage.setItem('selected', JSON.stringify(storeFilters))
 }
 
-// Set filters as selected
+// On-load selected filters
 function setSelected(index, target) {
     if (storeFilters[index] && JSON.parse(storeFilters[index]).selected === true) {
         target.classList.toggle('is-info')
+
     }
     if (storeFilters[6] && JSON.parse(storeFilters[6]).sliderOpen === true) {
         sliderC.remove('closed')
+        sliderC.add('opened')
     }
 }
+
 
 // Clear localStorage & selected filters
 function clearFilters() {
     storeFilters = []
     localStorage.clear()
-    filter.forEach((elem) => {
+    filters.forEach((elem) => {
         if (elem.classList.contains('is-info')) {
             elem.classList.toggle('is-info')
         }
     })
-    sliderOpen()
+    filtered = [];
+    storeSliderState()
 }
-
-// init
-filterSelect()
